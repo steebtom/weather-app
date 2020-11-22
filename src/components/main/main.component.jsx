8ace1e8e33de1df6db2384ed48dd85d830ff490b
steebtom <steeb.webdev@gmail.com>
2020-11-22 23:13:26 +1300

    git ignore

--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -6,6 +6,7 @@ import Header from "../header/header.component";
 const API_KEY = "3352729149c73266f5d23bbd95bca84d";
 const url = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
+const
 
 const Main = () => {
     return (
