8a50a4865e97802a4ced1f51679090149e86357a
steebtom <steeb.webdev@gmail.com>
2020-11-23 00:38:58 +1300

    context intro

--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -1,9 +1,12 @@
 import React,{ useState } from "react";
 import Axios from "axios";
 
+import Context from "../../Context"
+
 import Header from "../header/header.component";
 import Content from "../content/content.component";
 import Weathersearch from "../weathersearch/weathersearch.component";
+import Weatherdata from "../weatherdata/weatherdata.component";
 
 
 const API_KEY = "3352729149c73266f5d23bbd95bca84d";
@@ -17,13 +20,14 @@ const Main = () => {
 
         e.preventDefault();
 
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=${API_KEY}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=${API_KEY}&units=metric`;
         
         const request = Axios.get(url);
         const response = await request;
-        setWeather(response);
+        setWeather(response.data.main);
 
     }
+    weather && console.log(weather);
 
 
     return (
@@ -31,6 +35,7 @@ const Main = () => {
             <Header />
             <Content>
                 <Weathersearch api_call={ api_call } />
+                { weather && <Weatherdata weather={ weather } /> }
             </Content>
         </div>
     );
