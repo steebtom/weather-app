506826ce2f31bdeaab28da0d1584bd8d8fc6b84f
steebtom <steeb.webdev@gmail.com>
2020-11-22 23:10:06 +1300

    components 1 additional

--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 import Header from "../header/header.component";
 
+
 const API_KEY = "3352729149c73266f5d23bbd95bca84d";
 const url = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+
 
 const Main = () => {
     return (
