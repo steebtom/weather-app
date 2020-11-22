ab1767942cc296558b3ce43b746ebd8214a8aa27
steebtom <steeb.webdev@gmail.com>
2020-11-22 23:52:21 +1300

    components 2

---
+++ src/components/content/content.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Content = (props) => {
+
+    return(
+        <div className="content">
+            { props.children }
+        </div>
+    )
+}
+
+export default Content;
--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React,{ useState } from "react";
+import Axios from "axios";
 
 import Header from "../header/header.component";
+import Content from "../content/content.component";
+import Weathersearch from "../weathersearch/weathersearch.component";
 
 
 const API_KEY = "3352729149c73266f5d23bbd95bca84d";
-const url = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
-const
 
 const Main = () => {
+
+    const api_call = async e =>{
+
+        e.preventDefault();
+
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=${API_KEY}`;
+        
+        const request = Axios.get(url);
+        const response = await request;
+        console.log(response);
+
+    }
+
+
     return (
         <div className="main">
             <Header />
+            <Content>
+                <Weathersearch api_call={ api_call } />
+            </Content>
         </div>
     );
 }
