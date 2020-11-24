4cb396219fd53d4d3efeac1913f27e50bcc04263
steebtom <steeb.webdev@gmail.com>
2020-11-25 00:13:03 +1300

    build

---
+++ build/precache-manifest.b6db7fc148e3ca2119c22fef5da69f1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d8ce7839d26be8f3529b4ad870eb0d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "81db705d2346f1722843",
+    "url": "/weather-app/static/css/2.6a770793.chunk.css"
+  },
+  {
+    "revision": "56d52680695a89396a3a",
+    "url": "/weather-app/static/css/main.e4a23fa1.chunk.css"
+  },
+  {
+    "revision": "81db705d2346f1722843",
+    "url": "/weather-app/static/js/2.4a184d67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.4a184d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d52680695a89396a3a",
+    "url": "/weather-app/static/js/main.209492aa.chunk.js"
+  },
+  {
+    "revision": "8ad2e2c3d0b0209b3902",
+    "url": "/weather-app/static/js/runtime-main.4479c0a4.js"
+  }
+]);
